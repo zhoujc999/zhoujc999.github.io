5bc2f667a339768722a77abd3a6e058ca676748f
Jingchao Zhou <zhoujc999@gmail.com>
2023-06-04 01:32:00 +0000

    Testing routing

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/",
-  //       destination: "/home",
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <p className="mt-4 text-gray-500 dark:text-gray-400">
+      The page you are looking for doesn't exist. Here are some helpful links:
+    </p>
+  );
+}
