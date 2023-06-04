dce6e8a6d9a2e5e0b447ab2b8f7e5ade3354bbda
Jingchao Zhou <zhoujc999@gmail.com>
2023-06-04 02:11:31 +0000

    Fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <p className="mt-4 text-gray-500 dark:text-gray-400">
-      The page you are looking for doesn't exist. Here are some helpful links:
+      The page you are looking for does not exist. Here are some helpful links:
     </p>
   );
 }
