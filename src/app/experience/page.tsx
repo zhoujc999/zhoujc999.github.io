0fe530d1417cfafb4ee77492e4645cbeb9ec55dc
Jingchao Zhou <zhoujc999@gmail.com>
2023-06-25 09:05:34 +0000

    format

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -7,22 +7,120 @@ export default function Experience() {
   return (
     <div className="">
       <ol>
-        <li>
-          <div className="my-2 bg-blue-100">
-            <div className="flex flex-row items-center p-4 shrink-0">
+        <li className="my-4 first:mt-0 last:mb-0">
+          <div className="w-auto bg-blue-100 px-6 py-4">
+            <div className="my-2 flex shrink-0 flex-row items-center">
               <Image src="/nuro.jpg" alt="Logo" width={64} height={64} />
-              <ol className="px-2">
-                <li><MdWork className="h-6 w-6 mx-1 inline-block align-bottom" />Software Engineer</li>
-                <li><MdAlternateEmail className="h-6 w-6 mx-1 inline-block align-bottom" />Mastercard</li>
-                <li><span className="text-gray-500 mx-1">June 2021 – Present</span><span className="text-gray-500 mx-1 max-lg:hidden">|</span><span className="text-gray-500 mx-1 max-lg:hidden">Mountain View, CA</span></li>
+              <ol className="px-4">
+                <li>
+                  <MdWork className="mr-1 inline-block h-6 w-6 align-bottom" />
+                  Data Science & Engineering Intern
+                </li>
+                <li>
+                  <MdAlternateEmail className="mr-1 inline-block h-6 w-6 align-bottom" />
+                  Carta Healthcare
+                </li>
+                <li>
+                  <span className="mr-1 text-gray-500">
+                    May 2022 – Aug 2022
+                  </span>
+                  <span className="mx-1 text-gray-500 max-lg:hidden">|</span>
+                  <span className="mx-1 text-gray-500 max-lg:hidden">
+                    San Mateo, CA
+                  </span>
+                </li>
               </ol>
             </div>
-            <p>Defined and developed key metrics in data visualization dashboards; Collaborated with Product and Sales team to present dashboards at a client-facing Product Demo</p>
+            <div className="mx-3 my-2">
+              <ol className="list-disc">
+                <li className="py-0.5 first:pt-0 last:pb-0">
+                  Defined and developed key metrics in data visualization
+                  dashboards
+                </li>
+                <li className="py-0.5 first:pt-0 last:pb-0">
+                  Collaborated with Product and Sales team to present dashboards
+                  at a client-facing Product Demo
+                </li>
+                <li className="py-0.5 first:pt-0 last:pb-0">
+                  Implemented comprehensive & robust data cleaning process to
+                  reduce platform test errors systematically, reducing 30% of
+                  the time bioinformaticians spent on housekeeping tasks
+                </li>
+                <li className="py-0.5 first:pt-0 last:pb-0">
+                  Built, maintained, debugged infrastructure that streamlined
+                  and scaled 100x patient profile simulation based on real-world
+                  distribution to reduce manual labor
+                </li>
+                <li className="py-0.5 first:pt-0 last:pb-0">
+                  Designed SQLAlchemy wrappers to scalably query OLAP
+                  multi-dimensional array of data in PostgreSQL databases
+                </li>
+                <li className="py-0.5 first:pt-0 last:pb-0">
+                  Integrated FreeMarker API with machine learning pipeline to
+                  auto-generate free-text notes for NLP pre-training preparation
+                </li>
+              </ol>
+            </div>
           </div>
         </li>
-        <li className="my-2 h-24 bg-red-500">2</li>
-        <li className="my-2 h-24 bg-blue-500">3</li>
-        <li className="my-2 h-24 bg-red-500">4</li>
+        <li className="my-4 first:mt-0 last:mb-0">
+          <div className="w-auto bg-blue-100 px-6 py-4">
+            <div className="my-2 flex shrink-0 flex-row items-center">
+              <Image src="/nuro.jpg" alt="Logo" width={64} height={64} />
+              <ol className="px-4">
+                <li>
+                  <MdWork className="mr-1 inline-block h-6 w-6 align-bottom" />
+                  Summer Data Science Fellow
+                </li>
+                <li>
+                  <MdAlternateEmail className="mr-1 inline-block h-6 w-6 align-bottom" />
+                  Carta Healthcare
+                </li>
+                <li>
+                  <span className="mr-1 text-gray-500">
+                    May 2022 – Aug 2022
+                  </span>
+                  <span className="mx-1 text-gray-500 max-lg:hidden">|</span>
+                  <span className="mx-1 text-gray-500 max-lg:hidden">
+                    San Mateo, CA
+                  </span>
+                </li>
+              </ol>
+            </div>
+            <div className="mx-3 my-2">
+              <ol className="list-disc">
+                <li className="py-0.5 first:pt-0 last:pb-0">
+                  Defined and developed key metrics in data visualization
+                  dashboards
+                </li>
+                <li className="py-0.5 first:pt-0 last:pb-0">
+                  Collaborated with Product and Sales team to present dashboards
+                  at a client-facing Product Demo
+                </li>
+                <li className="py-0.5 first:pt-0 last:pb-0">
+                  Implemented comprehensive & robust data cleaning process to
+                  reduce platform test errors systematically, reducing 30% of
+                  the time bioinformaticians spent on housekeeping tasks
+                </li>
+                <li className="py-0.5 first:pt-0 last:pb-0">
+                  Built, maintained, debugged infrastructure that streamlined
+                  and scaled 100x patient profile simulation based on real-world
+                  distribution to reduce manual labor
+                </li>
+                <li className="py-0.5 first:pt-0 last:pb-0">
+                  Designed SQLAlchemy wrappers to scalably query OLAP
+                  multi-dimensional array of data in PostgreSQL databases
+                </li>
+                <li className="py-0.5 first:pt-0 last:pb-0">
+                  Integrated FreeMarker API with machine learning pipeline to
+                  auto-generate free-text notes for NLP pre-training preparation
+                </li>
+              </ol>
+            </div>
+          </div>
+        </li>
+        <li className="my-4 bg-blue-500 first:mt-0 last:mb-0">3</li>
+        <li className="my-4 bg-red-500 first:mt-0 last:mb-0">4</li>
       </ol>
     </div>
   );
